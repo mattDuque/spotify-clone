a8c8819c35838a21906b58e277741d69a269152a
Matheus Duque <masaohc@gmail.com>
2022-01-02 21:13:49 -0300

    configured spotify web api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { LOGIN_URL } from "../../../lib/spotify"
 
 export default NextAuth({
 
@@ -7,6 +8,8 @@ export default NextAuth({
         SpotifyProvider({
             clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            authorization: LOGIN_URL
         }),
     ],
+    secret: process.env.JWT_SECRET
 })
